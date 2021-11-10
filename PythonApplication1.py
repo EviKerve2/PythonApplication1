b69f26e1573518c5e44d3af32e1091e67f4594e0
opilane <opilane@HP-CZC6297B82>
2021-11-10 12:24:10 +0200

    Add project files.

---
+++ PythonApplication1.py
@@ -0,0 +1,25 @@
+def arithmetic(a: float,b:float,c:str):
+    """Lihtne kalkulaator
+    + - liitmine
+    - - lahutamine
+    * - korrutamine
+    / - jagamine
+    :param float a: Esimene arv
+    :param float b: Teine arv
+    :param str c: Aritmeetiline tehing
+    :rtype float:
+    """
+    if c=="+":
+        r=a+b
+    elif c=="-":
+        r=a-b
+    elif c=="*":
+        r=a*b
+    elif c=="/":
+        if b!=0:       
+            r=a/b
+        else:
+            print("Div0")
+    else:
+        print("Viga")
+    return r
